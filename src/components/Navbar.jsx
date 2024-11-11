4a4a98fb8835acf41c5099bf7a44fd0a3fa274af
Alok Kumar Jha <jhaalok1997@gmail.com>
2024-11-11 15:21:00 +0530

    topbar and navbar is completed...

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-start  space-x-24">
+        <div>
+            <img className="h-[120px] w-[249px] pl-[31px] " src="./logo.jpeg"/>
+        </div>
+        <div classname="">
+        <div className="bg-[#F5F5F5] shadow-md shadow-[#F5F5F5] pl-6 relative top-7 ">
+            <ul className="flex space-x-16 p-4 ">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Blog</li>
+                <li>Our Services</li>
+                <li>ILETS & PTE</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        </div>
+        <div className="relative top-4 p-4">
+            <button className="bg-slate-600  w-[139.6px] h-[40.24px] rounded-[4.72px] text-white">Login</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,19 @@
+
+
+const Topbar = () => {
+  return (
+    
+    <>
+        <div className="bg-[#641566] h-10 w-full">
+            <div>
+                <img  className ="flex float-end" src="./Container.png"/>
+            </div>
+
+        </div>
+
+    </>
+    
+  )
+}
+
+export default Topbar;
